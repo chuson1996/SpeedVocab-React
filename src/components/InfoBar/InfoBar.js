8d6e37f0f9dc85a03c210a75072c2153d93a2967
Chu Hoang Son <chuson1996@gmail.com>
2016-05-07 19:41:59 +0300

    First commit

--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -4,27 +4,27 @@ import {connect} from 'react-redux';
 import {load} from 'redux/modules/info';
 
 @connect(
-    state => ({info: state.info.data}),
-    dispatch => bindActionCreators({load}, dispatch))
+		state => ({info: state.info.data}),
+		dispatch => bindActionCreators({load}, dispatch))
 export default class InfoBar extends Component {
-  static propTypes = {
-    info: PropTypes.object,
-    load: PropTypes.func.isRequired
-  }
+	static propTypes = {
+		info: PropTypes.object,
+		load: PropTypes.func.isRequired
+	}
 
-  render() {
-    const {info, load} = this.props; // eslint-disable-line no-shadow
-    const styles = require('./InfoBar.scss');
-    return (
-      <div className={styles.infoBar + ' well'}>
-        <div className="container">
-          This is an info bar
-          {' '}
-          <strong>{info ? info.message : 'no info!'}</strong>
-          <span className={styles.time}>{info && new Date(info.time).toString()}</span>
-          <button className="btn btn-primary" onClick={load}>Reload from server</button>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const {info, load} = this.props; // eslint-disable-line no-shadow
+		const styles = require('./InfoBar.scss');
+		return (
+			<div className={styles.infoBar + ' well'}>
+				<div className="container">
+					This is an info bar
+					{' '}
+					<strong>{info ? info.message : 'no info!'}</strong>
+					<span className={styles.time}>{info && new Date(info.time).toString()}</span>
+					<button className="btn btn-primary" onClick={load}>Reload from server</button>
+				</div>
+			</div>
+		);
+	}
 }
